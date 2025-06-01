e76cad11a6a929dbb38476d1efb769396ec8557f
Chennakesava reddy Siddavatam <chennakesava9007@gmail.com>
2025-06-01 22:24:03 +0530

    Delete Positive or Negative number.py

--- Positive or Negative number.py
+++
@@ -1,8 +0,0 @@
-user_input = int(input("Enter a number: "))
-if user_input >0:
-    print(f"ohh! {user_input} is Positive number")
-elif user_input <0:
-    print(f"ohh! {user_input} is Negative number")
-else:
-    print("ohh! The number is Zero")
-# This code checks if the input number is positive, negative, or zero.
